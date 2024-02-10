603274bb755acaf552ab0b8f8d07ac75712c45ba
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-02-10 15:51:04 +0200

    finish landing and nav structure and styling

--- script/main.js
+++ script/main.js
@@ -1,1 +1,8 @@
-"use strict";
+// typed js
+const type = new Typed('.typed', {
+  strings: ['Software Engineer', 'Front-end Developer', 'Angular Developer'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
