6e29fb35699f8c81c8049ae9d948523abe1c5d8c
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2024-02-13 11:05:55 +0200

    add details of each project

--- script/main.js
+++ script/main.js
@@ -20,12 +20,35 @@ let mixer = mixitup('.work', {
 // activate project category
 document.querySelector('.filter').addEventListener('click', activeCategory);
 const categories = document.querySelectorAll('.filter li');
+
 function activeCategory(e) {
   categories.forEach((li) => {
     if (e.target.textContent === li.textContent) {
       e.target.classList.add('active-category');
-    } else {
+    } else if (
+      e.target.textContent !== li.textContent &&
+      !e.target.classList.contains('filter')
+    ) {
       li.classList.remove('active-category');
     }
   });
 }
+
+// display project details info
+const detailsBtn = document.querySelectorAll('.product-details'),
+  details = document.querySelectorAll('#product-details'),
+  closeDetails = document.querySelectorAll('.close');
+
+detailsBtn.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    e.target.nextElementSibling.style.display = 'block';
+    document.body.style.overflow = 'hidden';
+  });
+});
+
+closeDetails.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    e.target.parentElement.parentElement.style.display = 'none';
+    document.body.style.overflow = 'auto';
+  });
+});
